07945be8fd07dc4f034381874a2013ae8efcc0c5
dronperminov <perminov@ispras.ru>
2024-09-22 19:13:41 +0300

    remove movie by fact questions

--- src/enums/question_type.py
+++ src/enums/question_type.py
@@ -7,7 +7,6 @@ class QuestionType(Enum):
     MOVIE_BY_SLOGAN = "movie_by_slogan"
     MOVIE_BY_SHORT_DESCRIPTION = "movie_by_short_description"
     MOVIE_BY_DESCRIPTION = "movie_by_description"
-    MOVIE_BY_FACT = "movie_by_fact"
     MOVIE_BY_IMAGE = "movie_by_image"
     MOVIE_BY_ACTORS = "movie_by_actors"
     MOVIE_BY_CHARACTERS = "movie_by_characters"
@@ -19,7 +18,6 @@ class QuestionType(Enum):
             QuestionType.MOVIE_BY_SLOGAN: "КМС по слогану",
             QuestionType.MOVIE_BY_SHORT_DESCRIPTION: "КМС по короткому описанию",
             QuestionType.MOVIE_BY_DESCRIPTION: "КМС по описанию",
-            QuestionType.MOVIE_BY_FACT: "КМС по факту",
             QuestionType.MOVIE_BY_IMAGE: "КМС по кадру",
             QuestionType.MOVIE_BY_ACTORS: "КМС по актёрам",
             QuestionType.MOVIE_BY_CHARACTERS: "КМС по именам персонажей",
@@ -37,9 +35,6 @@ class QuestionType(Enum):
 
         if self == QuestionType.MOVIE_BY_DESCRIPTION:
             return {"description.text": {"$ne": ""}}
-
-        if self == QuestionType.MOVIE_BY_FACT:
-            return {"facts": {"$ne": []}}
 
         if self == QuestionType.MOVIE_BY_IMAGE:
             return {"image_urls": {"$ne": []}}
